55c4bb44f9b33e1266f3420f375ef1d06f4720e8
Brandon Harrell <BHarrell@myrgroup.com>
2018-11-20 14:12:45 -0700

    gif changes

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -102,8 +102,19 @@ $(document).ready(function () {
     removeLastButton();
 
     $(document).on("click", ".action", displayGifs);
+    $(".image").on("click", ".image", function() {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+
 
 
 });
 
+});
 
