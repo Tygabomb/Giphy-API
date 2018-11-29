e02bf7d0161abe7f3dc102a2214b649d602e2ee6
Tyga <b_harrell17@outlook.com>
2018-11-28 17:16:37 -0700

    final touches

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
             gifButton.attr("data-name", myGifs[i]);
             gifButton.text(myGifs[i]);
             $("#gifButtonsArea").append(gifButton);
-            console.log('%c myGifs.length', "color: blue; font-wieght: bold;")
+            console.log(`%c ${myGifs.length}, "color: blue; font-wieght: bold;`)
         }
     }
 
@@ -46,12 +46,14 @@ $(document).ready(function () {
 
         // AJAX for Giphy API
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=JzqMXIEKUptS1dOLIQWCNf4Fs51dUmoJ";
-        console.log('%c queryURL', "color: orange; font-wieght: bold;")
+        console.log(`%c ${queryURL} "color: orange; font-wieght: bold;`);
+        console.log(queryURL);
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             console.log('%c response', 'color: red; font-weight: bold;');
+            console.log(response);
             $("#gifsArea").empty();
 
             //shows results of gifs
@@ -65,10 +67,16 @@ $(document).ready(function () {
                 gifDiv.addClass("gifDiv");
 
                 // pulling rating of gif
-                var gifRating = $("<p>").html("Rating: " + results[i].Rating);
+                var gifRating = $("<p>").html("Rating: " + results[i].rating);
                 gifDiv.append(gifRating);
+                console.log(results[i]);
 
+
+                // this section sent me to the main giphy site but still couldn't download 
                 // pulling gif
+                gifA = $("<a download>")
+                //gifA.attr('download', '')
+                // gifA.attr('href', results[i].images.fixed_height_small.url)
                 var gifImage = $("<img>");
 
     
@@ -77,38 +85,43 @@ $(document).ready(function () {
                 gifImage.attr("data-state", "still");
                 gifImage.attr("src", results[i].images.fixed_height_small_still.url);
                 gifImage.addClass("image");
-                gifDiv.append(gifImage);
+                gifA.append(gifImage);
+                gifDiv.append(gifA);
                 
 
                 
                 // adding div of gifs to gifsArea div
                 $("#gifsArea").prepend(gifDiv);
-                console.log('%c gifDiv', 'color: green; font-weight: bold;');
+                console.log(`%c ${gifDiv}, 'color: green; font-weight: bold;`);
+                console.log(gifDiv);
             }
-            function downloadGifBtn(){
-                $("#download").on("click", function(){
+            // function downloadGifBtn(){
+            //     $("#download").on("click", function(){
                 
                     
-                });
-            }        
+            //     });
+            // }        
     
 
         });
     }
     // function to save the giff externally 
-    document.onclick = function (e) {
-        e = e || window.event;
-        var element = e.target || e.srcElement;
-        if (element.innerHTML == "Image") {
-            //someFunction(element.href);
-            var name = element.nameProp;
-            var address = element.href;
-            saveImageAs1(element.nameProp, element.href);
-            return false; // Prevent default action and stop event propagation
-        }
-        else
-            return true;
-    };
+
+    // $("#download").on("click", function(){
+    // document.onclick = function (e) {
+    //     e = e || window.event;
+    //     var element = e.target || e.srcElement;
+    //     if (element.innerHTML == "image") {
+    //         //someFunction(element.href);
+    //         var name = element.nameProp;
+    //         var address = element.href;
+    //         saveImageAs1(element.nameProp, element.href);
+    //         return false; // Prevent default action and stop event propagation
+    //     }
+    //     else
+    //         return true;
+    // };
+    // });
 
     function saveImageAs1(name, adress) {
         var SaveGiff = $("#download").val().trim();
@@ -119,7 +132,7 @@ $(document).ready(function () {
             setTimeout('win.document.execCommand("SaveAs")', 100);
             setTimeout('win.close()', 500);
         }
-    }
+    };
 
 
     // allways remember to call out functions 
